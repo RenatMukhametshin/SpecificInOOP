0200f2524c1d0474fa00015960b6141f725ea7ac
RenatOnDemoStand <myemail@mydomain.com>
2021-08-05 12:49:53 +0500

    added example lambda expressions for sorting

---
+++ src/prj01LambdaExpression/Employee.java
@@ -0,0 +1,54 @@
+package prj01LambdaExpression;
+
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+
+public class Employee //implements Comparable<Employee>
+{
+    private String name;
+    private Integer salary;
+    private Date workStart;
+
+    public Employee(String name, Integer salary, Date workStart)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.workStart = workStart;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Date getWorkStart() {
+        return workStart;
+    }
+
+    public void setWorkStart(Date workStart) {
+        this.workStart = workStart;
+    }
+
+    public String toString()
+    {
+        return name + " - " + salary + " - " +
+            (new SimpleDateFormat("dd.MM.yyyy")).format(workStart);
+    }
+
+//    @Override
+//    public int compareTo(Employee employee) {
+//        return getName().compareTo(employee.getName());
+//    }
+}
---
+++ src/prj01LambdaExpression/Main.java
@@ -0,0 +1,65 @@
+package prj01LambdaExpression;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = loadStaffFromFile();
+
+        Collections.sort(staff, (employee1, employee2) -> employee1.getSalary().compareTo(employee2.getSalary()) );
+
+        System.out.println("\nSorted salary");
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+
+        System.out.println("\nSorted salary :: name");
+        Collections.sort(staff,
+                (employee1, employee2) ->
+                        {
+                            int resultCompareSalary = employee1.getSalary().compareTo(employee2.getSalary());
+                            int resultCompareName = employee1.getName().compareTo(employee2.getName());
+                            if ( resultCompareSalary == 0)
+                                return resultCompareName;
+                            else
+                                return resultCompareSalary;
+                        }
+        );
+
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+
+
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
